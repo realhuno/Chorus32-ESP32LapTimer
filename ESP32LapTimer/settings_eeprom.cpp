4e4d865cc0757884d0bb3350746ce4bb5c095f18
Kevin <smeatsan@gmail.com>
2019-07-09 12:43:30 +0200

    Remove extern variables in eeprom settings.

--- ESP32LapTimer/settings_eeprom.cpp
+++ ESP32LapTimer/settings_eeprom.cpp
@@ -8,12 +8,9 @@ struct EepromSettingsStruct EepromSettings;
 
 ///////////Extern Variable we need acces too///////////////////////
 
-static RXADCfilter_ RXADCfilter;
-static ADCVBATmode_ ADCVBATmode;
 static bool eepromSaveRequired = false;
 
 uint8_t NumRecievers = 6;
-extern float VBATcalibration;
 
 //////////////////////////////////////////////////////////////////
 
@@ -149,23 +146,23 @@ bool EepromSettingsStruct::validateCRC(){
 
 
 RXADCfilter_ getRXADCfilter() {
-  return RXADCfilter;
+	return EepromSettings.RXADCfilter;
 }
 
 ADCVBATmode_ getADCVBATmode() {
-  return ADCVBATmode;
+	return EepromSettings.ADCVBATmode;
 }
 
 void setRXADCfilter(RXADCfilter_ filter) {
-  RXADCfilter = filter;
+	EepromSettings.RXADCfilter = filter;
 }
 
 void setADCVBATmode(ADCVBATmode_ mode) {
-  ADCVBATmode = mode;
+	EepromSettings.ADCVBATmode = mode;
 }
 
 void setSaveRequired() {
-  eepromSaveRequired = true;
+	eepromSaveRequired = true;
 }
 
 int getWiFiChannel(){
