becec8cacfe603842cc273d3116282cd87ca5b82
Kevin <smeatsan@gmail.com>
2019-07-09 12:43:54 +0200

    Add support for LoRa communication.

--- ESP32LapTimer/targets/config_ttgo_lora_v1.h
+++ ESP32LapTimer/targets/config_ttgo_lora_v1.h
@@ -1,4 +1,4 @@
-#define BEEPER 14
+#define BEEPER 4
 
 #define LEDs 2
 
@@ -7,12 +7,15 @@
 #define BUTTON1 13
 #define BUTTON2 12
 
+#define MOSI 23
+#define SCK 25
+
 #define CS1 16
-#define CS2 5
+#define CS2 4 
 #define CS3 4
-#define CS4 15
-#define CS5 25
-#define CS6 26
+#define CS4 4
+#define CS5 4
+#define CS6 4
 
 #define ADC1 ADC1_CHANNEL_0
 #define ADC2 ADC1_CHANNEL_3
@@ -27,3 +30,12 @@
 #define ADC4_GPIO 33
 #define ADC5_GPIO 32
 #define ADC6_GPIO 35
+
+#define USE_LORA
+#define LORA_SCK 5
+#define LORA_MISO 19
+#define LORA_MOSI 27
+#define LORA_CS 18
+#define LORA_RST 14
+#define LORA_IRQ 26
+#define LORA_FREQ 866E6
