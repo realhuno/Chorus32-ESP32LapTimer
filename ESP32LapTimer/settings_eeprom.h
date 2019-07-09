897c42cbdcdfbef3c7c9b0e3af5e5b8b0232935e
Kevin <smeatsan@gmail.com>
2019-07-09 12:43:41 +0200

    Fix module calls and remove unused variable from eeprom.

--- ESP32LapTimer/settings_eeprom.h
+++ ESP32LapTimer/settings_eeprom.h
@@ -21,7 +21,6 @@ struct EepromSettingsStruct {
   uint16_t eepromVersionNumber;
   uint8_t RXBand[MAX_NUM_PILOTS];
   uint8_t RXChannel[MAX_NUM_PILOTS];
-  uint16_t RXfrequencies[MAX_NUM_PILOTS];
   uint16_t RSSIthresholds[MAX_NUM_PILOTS];
   RXADCfilter_ RXADCfilter;
   ADCVBATmode_ ADCVBATmode;
