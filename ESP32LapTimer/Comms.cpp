f19b9092ceb179e404a4a2cb546980f4e1d15d1c
Kevin <smeatsan@gmail.com>
2019-07-09 12:43:45 +0200

    Fix selecting skip first lap in app.

--- ESP32LapTimer/Comms.cpp
+++ ESP32LapTimer/Comms.cpp
@@ -465,7 +465,7 @@ void WaitFirstLap(uint8_t NodeAddr) {
   addToSendQueue('S');
   addToSendQueue(TO_HEX(NodeAddr));
   addToSendQueue(RESPONSE_WAIT_FIRST_LAP);
-  addToSendQueue(TO_HEX(getSkipFirstLap()));
+  addToSendQueue(TO_HEX(getCountFirstLap()));
   addToSendQueue('\n');
 }
 
@@ -651,7 +651,8 @@ void handleSerialControlInput(char *controlData, uint8_t  ControlByte, uint8_t N
 
       case CONTROL_WAIT_FIRST_LAP:
         valueToSet = TO_BYTE(controlData[3]);
-        setSkipFirstLap(valueToSet);
+        setCountFirstLap(valueToSet);
+        WaitFirstLap(0);
         //playClickTones();
         isConfigured = 1;
         break;
