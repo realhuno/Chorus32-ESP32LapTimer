d6a652c62f13d126504322add334db8084d140b8
Kevin <smeatsan@gmail.com>
2019-07-09 12:43:29 +0200

    Add crc for eeprom. Use struct for defaults.

--- ESP32LapTimer/settings_eeprom.cpp
+++ ESP32LapTimer/settings_eeprom.cpp
@@ -2,6 +2,7 @@
 #include "settings_eeprom.h"
 #include "Comms.h"
 #include "RX5808.h"
+#include "crc.h"
 
 struct EepromSettingsStruct EepromSettings;
 
@@ -112,7 +113,7 @@ bool EepromSettingsStruct::SanityCheck() {
       return IsGoodEEPROM;
     }
   }
-  return IsGoodEEPROM;
+  return IsGoodEEPROM && this->validateCRC();
 }
 
 void EepromSettingsStruct::save() {
@@ -145,12 +146,29 @@ void EepromSettingsStruct::defaults() {
   settings.NumRecievers = 6;
   settings.WiFiProtocol = 1;
   settings.WiFiChannel = 1;
+  settings.updateCRC();
 
   *this = settings;
 
   EEPROM.put(0, *this);
   EEPROM.commit();
 }
+
+crc_t EepromSettingsStruct::calcCRC() {
+	crc_t crc = crc_init();
+	crc = crc_update(crc, this, sizeof(*this) - sizeof(this->crc));
+	crc = crc_finalize(crc);
+	return crc;
+}
+
+void EepromSettingsStruct::updateCRC() {
+	this->crc = this->calcCRC();
+}
+
+bool EepromSettingsStruct::validateCRC(){
+	return this->crc == this->calcCRC();
+}
+
 
 
 RXADCfilter_ getRXADCfilter() {
