1d08b31c7bb53ea9915193f51d344bb5268b8c9a
Kevin <smeatsan@gmail.com>
2019-07-17 00:52:59 +0200

    Only power the bare minimum for getting rssi.
    Make low power a compile time option.
    Low power increases the tune time and makes multiplexing worse.

--- ESP32LapTimer/src/HardwareConfig.h
+++ ESP32LapTimer/src/HardwareConfig.h
@@ -57,4 +57,7 @@
 #ifndef EEPROM_DEFAULT_MIN_VOLTAGE_MODULE
 #define EEPROM_DEFAULT_MIN_VOLTAGE_MODULE 6000
 #endif
+
+//#define USE_LOW_POWER // this saves about 5-10mA but the tune time of the module is worse
+
 void InitHardwarePins();
