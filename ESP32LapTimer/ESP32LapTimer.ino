28409e0a5660c519f59eb422ac1dd07c2cb9f59f
Kevin <smeatsan@gmail.com>
2019-07-16 23:46:15 +0200

    Power down all modules on startup.

--- ESP32LapTimer/ESP32LapTimer.ino
+++ ESP32LapTimer/ESP32LapTimer.ino
@@ -88,7 +88,7 @@ void setup() {
   ConfigureADC();
 
   InitSPI();
-  //PowerDownAll(); // Powers down all RX5808's
+  RXPowerDownAll(); // Powers down all RX5808's
   delay(250);
 
   InitWifiAP();
