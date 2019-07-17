43916533c7216490a6fe27ee8e09f3d814f5b36b
Kevin <smeatsan@gmail.com>
2019-07-17 20:27:41 +0200

    Small cleanup in ADC.

--- ESP32LapTimer/ADC.cpp
+++ ESP32LapTimer/ADC.cpp
@@ -26,11 +26,8 @@ static Timer ina219Timer = Timer(1000);
 
 static Adafruit_INA219 ina219; // A0+A1=GND
 
-static uint32_t LastADCcall;
-
 static esp_adc_cal_characteristics_t adc_chars;
 
-static unsigned int VbatReadingSmooth;
 static uint16_t adcLoopCounter = 0;
 
 static float VBATcalibration;
@@ -132,11 +129,9 @@ static IRAM_ATTR bool setNextPilot(uint8_t adc) {
 }
 
 void ConfigureADC() {
-	
 	pilot_queue_lock = xSemaphoreCreateMutex();
 	pilots_lock = xSemaphoreCreateMutex();
 
-	
 	memset(pilots, 0, MAX_NUM_PILOTS * sizeof(pilot_data_t));
 	for(int i = 0; i < MAX_NUM_PILOTS; ++i) {
 		pilots[i].number = i;
@@ -204,7 +199,6 @@ adc1_channel_t IRAM_ATTR getADCChannel(uint8_t adc_num) {
 void IRAM_ATTR nbADCread( void * pvParameters ) {
 	static uint8_t current_adc = 0;
 	uint32_t now = micros();
-	LastADCcall = now;
 	if(xSemaphoreTake(pilots_lock, 1)) { // lock changes to pilots from other cores 
 		if(now - receivers[current_adc].last_hop > MULTIPLEX_STAY_TIME_US + MIN_TUNE_TIME_US && isRxReady(current_adc)) {
 			if(setNextPilot(current_adc)) {
@@ -251,9 +245,10 @@ void IRAM_ATTR nbADCread( void * pvParameters ) {
 				if(current_pilot->number == 0) ++adcLoopCounter;
 			}
 		} // end if isRxReady
-		// use minimum here to ensure the first n receivers are used since the other ones might be offline in case we have more rx than pilots
 		xSemaphoreGive(pilots_lock);
 	}
+	// use minimum here to ensure the first n receivers are used since the other ones might be offline in case we have more rx than pilots
+	// use max to avoid division by 0
 	current_adc = (current_adc + 1) % MAX(1, MIN(getNumReceivers(), current_pilot_num));
 }
 
@@ -328,11 +323,11 @@ float getVbatFloat(bool force_read){
 	if((micros() - last_voltage_update) > VOLTAGE_UPDATE_INTERVAL_US || force_read) {
 		switch (getADCVBATmode()) {
 			case ADC_CH5:
-				VbatReadingSmooth = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC5), &adc_chars);
+				uint32_t VbatReadingSmooth = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC5), &adc_chars);
 				setVbatFloat(VbatReadingSmooth / 1000.0 * VBATcalibration);
 				break;
 			case ADC_CH6:
-				VbatReadingSmooth = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC6), &adc_chars);
+				uint32_t VbatReadingSmooth = esp_adc_cal_raw_to_voltage(adc1_get_raw(ADC6), &adc_chars);
 				setVbatFloat(VbatReadingSmooth / 1000.0 * VBATcalibration);
 				break;
 			case INA219:
