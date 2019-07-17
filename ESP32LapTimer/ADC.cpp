6219a4a0552ae8d927ef484d5e962baa96dc414a
Kevin <smeatsan@gmail.com>
2019-07-17 14:22:15 +0200

    Add support for disabling multiplexing on a per pilot basis.

--- ESP32LapTimer/ADC.cpp
+++ ESP32LapTimer/ADC.cpp
@@ -61,6 +61,7 @@ typedef struct pilot_data_s {
 	receiver_data_t* current_rx;
 	uint8_t number;
 	uint32_t unused_time; // used to force a certain stay time. if we allow an instant switch, modules would be constantly switching with e.g. 6 modules and 7 pilots
+	bool disable_multiplexing; // don't switch this pilot. scenario: 3 modules installed with 4 pilots. 2 pilots really care about their time and the other 2 just want to see their approximate time. so fix the first two pilots and the remaining module multiplexes the other 2
 } pilot_data_t;
 
 typedef struct receiver_data_s {
@@ -91,6 +92,10 @@ static uint16_t multisample_adc1(adc1_channel_t channel, uint8_t samples) {
  * \returns if a different pilot was found.
  */
 static IRAM_ATTR bool setNextPilot(uint8_t adc) {
+	// skip this receiver if the assigned pilot is fixed
+	if(receivers[adc].current_pilot && receivers[adc].current_pilot->disable_multiplexing) {
+		return false;
+	}
 	if(!xSemaphoreTake(pilot_queue_lock, 1)) {
 		// failed to obtain mutex, so do nothing
 		return false;
