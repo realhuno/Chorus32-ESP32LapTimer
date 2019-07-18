562095af28c8cfecde6a14c6338eb202e2a47c17
Kevin <smeatsan@gmail.com>
2019-07-18 21:36:53 +0200

    Make drawing the magnitude graph an argument

--- scripts/analyze_debug_data.py
+++ scripts/analyze_debug_data.py
@@ -15,7 +15,7 @@ class lowpass_filter:
 		self.state = 0
 
 	def  add_value(self, value: float) -> float:
-	# y[i] := y[i-1] + α * (x[i] - y[i-1])
+		# y[i] := y[i-1] + α * (x[i] - y[i-1])
 		self.state =  self.state + self.alpha * (value - self.state)
 		return self.state
 
@@ -44,7 +44,7 @@ def mask_data(data, threshold, grace_samples):
 	
 	return (upper_data, lower_data)
 
-def plot_data(data: List[int], threshold, grace_samples, lowpass: lowpass_filter):
+def plot_data(data: List[int], threshold, grace_samples, lowpass: lowpass_filter, plot_magnitude:bool = False):
 	raw_data = np.array(data)
 	lowpass.state = raw_data[0]
 	
@@ -65,10 +65,8 @@ def plot_data(data: List[int], threshold, grace_samples, lowpass: lowpass_filter
 		
 		i += 1
 	
-	normalized_data = raw_data - np.mean(raw_data)
 	t = np.arange(0.0, len(raw_data), 1)
 
-	fig, ax = plt.subplots(2, 1)
 	# not used to be more flexible with sample num
 	#upper_data = np.ma.masked_where(raw_data >= threshold, raw_data)
 	#lower_data = np.ma.masked_where(raw_data < threshold, raw_data)
@@ -76,17 +74,22 @@ def plot_data(data: List[int], threshold, grace_samples, lowpass: lowpass_filter
 	upper_data, lower_data = mask_data(raw_data, threshold, grace_samples)
 	upper_data_filtered, lower_data_filtered = mask_data(filtered_data, threshold, grace_samples)
 	
-	
-	ax[0].plot(t, lower_data, t, upper_data)
-	ax[0].plot(t, lower_data_filtered, t, upper_data_filtered)
-	ax[0].axhline(y=threshold, color='r')
-	ax[0].axvline(x=raw_max[1], color='r', label="raw max: difference {0:.2f}ms".format(abs(raw_max[1] - filtered_max[1]) * (1/args.sampling) * 1000))
-	ax[0].axvline(x=filtered_max[1], label="filtered max: difference {} samples".format(abs(raw_max[1] - filtered_max[1])))
-	ax[0].set_xlabel('Sample')
-	ax[0].set_ylabel('Amplitude')
-	ax[0].legend()
+	if plot_magnitude:
+		fig, ((ax_data, ax_magitude)) = plt.subplots(2, 1)
+		normalized_data = raw_data - np.mean(raw_data)
+		ax_magitude.magnitude_spectrum(normalized_data, Fs=args.sampling, scale=args.scale)
+	else:
+		fig, ax_data = plt.subplots(1, 1)
+		
+	ax_data.plot(t, lower_data, t, upper_data)
+	ax_data.plot(t, lower_data_filtered, t, upper_data_filtered)
+	ax_data.axhline(y=threshold, color='r')
+	ax_data.axvline(x=raw_max[1], color='r', label="raw max: difference {0:.2f}ms".format(abs(raw_max[1] - filtered_max[1]) * (1/args.sampling) * 1000))
+	ax_data.axvline(x=filtered_max[1], label="filtered max: difference {} samples".format(abs(raw_max[1] - filtered_max[1])))
+	ax_data.set_xlabel('Sample')
+	ax_data.set_ylabel('Amplitude')
+	ax_data.legend()
 
-	ax[1].magnitude_spectrum(normalized_data, Fs=args.sampling, scale=args.scale)
 
 if __name__ == "__main__":
 	parser = ArgumentParser(formatter_class=RawTextHelpFormatter)
@@ -95,6 +98,7 @@ if __name__ == "__main__":
 	parser.add_argument("-x", "--scale", dest="scale", help="Scaling of the frequency graph (dB, linear) [default: %(default)]", type=str, default="dB")
 	parser.add_argument("-t", "--threshold", dest="threshold", help="Threshold for coloring (like shown in the app) [default: %(default)]", type=int, default=150)
 	parser.add_argument("-c", "--cutoff", dest="cutoff", help="Lowpass filter cutoff [default: %(default)]", type=int, default=20)
+	parser.add_argument("-m", "--magnitude", dest="enable_magnitude", help="Enable magnitude graph [default: %(default)]", action='store_true')
 	args = parser.parse_args()
 
 	data_list = []
@@ -113,5 +117,5 @@ if __name__ == "__main__":
 	print("Got {} sets of data".format(len(data_list)))
 
 	for data in data_list:
-		plot_data(data, args.threshold * 12, 400, lowpass_filter(args.cutoff, 1/args.sampling))
+		plot_data(data, args.threshold * 12, 500, lowpass_filter(args.cutoff, 1/args.sampling), args.enable_magnitude)
 	plt.show()
