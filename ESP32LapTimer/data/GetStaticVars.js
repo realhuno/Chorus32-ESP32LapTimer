9a5a022c6ef7302b18a51f6444a4bf96d65e3bfb
Kevin <smeatsan@gmail.com>
2019-07-21 16:10:46 +0200

    Remove jquery dependency to make the SPIFFS < 190kb

--- ESP32LapTimer/data/GetStaticVars.js
+++ ESP32LapTimer/data/GetStaticVars.js
@@ -48,7 +48,7 @@
 
 			// channel selection
 			cell = row.insertCell(-1);
-			input_html = "<select name=\"channel" + i + "\" style=\"width: 100%;\"><option value=\"0\">1</option><option value=\"1\">2</option><option value=\"2\">3</option><option value=\"3\">4</option><option value=\"4\">5</option><option value=\"5\">6</option><option value=\"6\">7</option><option value=\"7\">8</option></select>";
+			input_html = "<select name=\"channel" + i + "\" id=\"channel" + i + "\" style=\"width: 100%;\"><option value=\"0\">1</option><option value=\"1\">2</option><option value=\"2\">3</option><option value=\"3\">4</option><option value=\"4\">5</option><option value=\"5\">6</option><option value=\"6\">7</option><option value=\"7\">8</option></select>";
 			cell.innerHTML = input_html;
 			
 			// RSSI threshold
@@ -78,7 +78,7 @@
 
     function updateBandChannel(data){
         for(var i=0;i<data.pilot_data.length;i++){
-            $('#band'+i).val(data.pilot_data[i].band);
-            $('#channel'+i).val(data.pilot_data[i].channel);
+            document.getElementById('band'+i).value = data.pilot_data[i].band;
+            document.getElementById('channel'+i).value = data.pilot_data[i].channel;
         }
     }
