d77c933b209c0b93ad6780120e984277893a2666
Kevin <smeatsan@gmail.com>
2019-07-25 16:43:32 +0200

    Only use new trigger mode when experimental is activated.

--- ESP32LapTimer/src/Comms.h
+++ ESP32LapTimer/src/Comms.h
@@ -16,5 +16,6 @@ void handleSerialControlInput(char *controlData, uint8_t  ControlByte, uint8_t N
 bool isInRaceMode();
 void startRace();
 void stopRace();
+bool isExperimentalModeOn();
 
 #endif // __COMMS_H__
