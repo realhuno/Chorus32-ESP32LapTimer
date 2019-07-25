ebafaed7a4fa095247f999dbd16e6c3d5944c260
Kevin <smeatsan@gmail.com>
2019-07-25 14:49:06 +0200

    Fix applying band and channel without multiplexing.

--- ESP32LapTimer/ADC.h
+++ ESP32LapTimer/ADC.h
@@ -42,3 +42,6 @@ bool isPilotMultiplexOff(uint8_t pilot);
 void setilotMultiplexOff(uint8_t pilot, bool off);
 void setPilotFilters(uint16_t cutoff);
 
+void setPilotBand(uint8_t pilot, uint8_t band);
+void setPilotChannel(uint8_t pilot, uint8_t channel);
+
