187b7e91b47589a60d785313406b95bf1f841f8e
Kevin <smeatsan@gmail.com>
2019-08-04 19:52:38 +0200

    Add LapRelay program. Used as a relay for LoRa.

---
+++ LapRelay/HardwareConfig.h
@@ -0,0 +1,20 @@
+
+#define MAX_UDP_CLIENTS 5
+#define WIFI_AP_NAME "Chorus32 LapTimer Relay"
+#define BLUETOOTH_NAME WIFI_AP_NAME
+
+#define USE_BLUETOOTH
+//#define USE_SERIAL
+
+//#define OUTPUT_DEBUG
+//#define INPUT_DEBUG
+
+
+#define USE_LORA
+#define LORA_SCK 5
+#define LORA_MISO 19
+#define LORA_MOSI 27
+#define LORA_CS 18
+#define LORA_RST 14
+#define LORA_IRQ 26
+#define LORA_FREQ 866E6
