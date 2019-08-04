657b872999712cc2f7339d1dd351e5f4e352a5e7
Kevin <smeatsan@gmail.com>
2019-08-04 19:52:06 +0200

    First working LoRa version. Still not very good and bad half-duplex and buffer handling.

--- ESP32LapTimer/src/targets/config_default.h
+++ ESP32LapTimer/src/targets/config_default.h
@@ -1,6 +1,6 @@
-#define BEEPER 14
+#define BEEPER 0
 
-#define LEDs 12
+#define LEDs 0
 
 #define BUTTON1 T7 // 27
 #define BUTTON2 T4 // 13
@@ -9,14 +9,14 @@
 #define CS2 5
 #define CS3 4
 #define CS4 15
-#define CS5 25
-#define CS6 26
+#define CS5 0
+#define CS6 0
 
 #define ADC1 ADC1_CHANNEL_0
 #define ADC2 ADC1_CHANNEL_3
 #define ADC3 ADC1_CHANNEL_6
 #define ADC4 ADC1_CHANNEL_7
-#define ADC5 ADC1_CHANNEL_4
+#define ADC5 ADC1_CHANNEL_7
 #define ADC6 ADC1_CHANNEL_5
 
 #define ADC1_GPIO 36
@@ -25,3 +25,12 @@
 #define ADC4_GPIO 35
 #define ADC5_GPIO 32
 #define ADC6_GPIO 33
+
+#define USE_LORA
+#define LORA_SCK 17
+#define LORA_MISO 32
+#define LORA_MOSI 26
+#define LORA_CS 12
+#define LORA_RST 14
+#define LORA_IRQ 25
+#define LORA_FREQ 866E6
