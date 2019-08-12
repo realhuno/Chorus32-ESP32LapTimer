4d12e8580f64d12b94742686e5383689eb2aaed4
Kevin <smeatsan@gmail.com>
2019-08-12 14:57:04 +0200

    Add dev target for me.

--- ESP32LapTimer/src/targets/target.h
+++ ESP32LapTimer/src/targets/target.h
@@ -4,6 +4,8 @@
   #include "config_old.h"
 #elif BOARD==BOARD_TTGO_LORA
   #include "config_ttgo_lora_v1.h"
+#elif BOARD==BOARD_DEV_BOARD
+  #include "config_dev_board.h"
 #else 
   #error "Unknown board!"
 #endif
