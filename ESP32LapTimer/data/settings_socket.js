c65f0a0dc1b4645396c974bc134bfa2f8c8a384b
Kevin <smeatsan@gmail.com>
2019-08-21 12:47:56 +0200

    Add more responsive settings for adc Others will follow.

---
+++ ESP32LapTimer/data/settings_socket.js
@@ -0,0 +1,80 @@
+import * as constants from './constants.js';
+
+var ws = null;
+
+function send_extended_data(type, data, bits) {
+	ws.send(`ER*${type}${(data).toString(bits).padStart(bits/4, '0').toUpperCase()}\n`);
+}
+
+function startWebsocket(websocketServerLocation){
+    ws = new WebSocket(websocketServerLocation);
+    ws.onmessage = on_websocket_event;
+    ws.onclose = function(){
+        // Try to reconnect in 5 seconds
+        setTimeout(function(){startWebsocket(websocketServerLocation)}, 5000);
+    };
+	ws.onopen = function() {
+		ws.send("ER*a\n"); // get all extended settings
+		ws.send("R*a\n"); // get all normal settings
+	};
+}
+
+function set_value_pending(object) {
+	object.style.border = "1px solid orange";
+}
+
+function set_value_received(object) {
+	object.style.border = "1px solid green";
+}
+
+
+function handle_message(message) {
+	var extended = message[0] == 'E';
+	if(extended) {
+		message = message.substr(1);
+	}
+	var pilot_num = parseInt(message[1]);
+	var cmd = message[2];
+
+	// Extended commands
+	if(extended) {
+		switch(cmd) {
+			case constants.EXTENDED_VOLTAGE_TYPE:
+				var field = document.getElementById("ADCVBATmode");
+				field.value = parseInt(message[3], 16);
+				set_value_received(field);
+				break;
+			case constants.EXTENDED_VOLTAGE_CALIB:
+				var field = document.getElementById("ADCcalibValue");
+				field.value = parseInt(message.substr(3), 16) / 1000.0;
+				set_value_received(field);
+				break;
+		}
+	} else {
+		switch(cmd) {
+		}
+	}
+}
+
+function on_websocket_event(event) {
+	console.log(event.data);
+	var messages = event.data.split('\n');
+	for(var i = 0; i < messages.length; ++i) {
+		handle_message(messages[i]);
+	}
+}
+
+startWebsocket("ws://192.168.4.1/ws");
+
+document.getElementById("ADCVBATmode").oninput = function() {
+	set_value_pending(this);
+	ws.send(`ER*${constants.EXTENDED_VOLTAGE_TYPE}${this.value}\n`);
+};
+document.getElementById("ADCcalibValue").oninput = function() {
+	set_value_pending(this);
+	if(this.value.endsWith('.')) {
+		this.style.border = "1px solid red";
+		return;
+	}
+	send_extended_data(constants.EXTENDED_VOLTAGE_CALIB, parseInt(this.value * 1000), 16);
+};
