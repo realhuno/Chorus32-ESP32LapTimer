66bdb1136caa7843736075926dda60afb8ee7728
Kevin <smeatsan@gmail.com>
2019-08-24 16:27:06 +0200

    Use more template strings

--- ESP32LapTimer/data/settings_socket.js
+++ ESP32LapTimer/data/settings_socket.js
@@ -153,7 +153,7 @@ function create_pilots() {
 		var input_html = "";
 		// band selection
 		cell = row.insertCell(-1);
-		input_html = "<select class=\"band_select\" name=\"band" + i + "\" id=\"band" + i + "\">";
+		input_html = `<select class="band_select" name="band${i}" id="band${i}">`;
 		input_html += "<option selected=\"\" value=\"0\">R</option><option value=\"1\">A</option><option value=\"2\">B</option><option value=\"3\">E</option><option value=\"4\">F</option><option value=\"5\">D</option><option value=\"6\">Connex</option><option value=\"7\">Connex2</option></select>";
 		cell.innerHTML = input_html;
 		cell.lastChild.onclick = function () {
@@ -162,7 +162,8 @@ function create_pilots() {
 		}
 		// channel selection
 		cell = row.insertCell(-1);
-		input_html = "<select name=\"channel" + i + "\" id=\"channel" + i + "\" class=\"channel_select\"><option value=\"0\">1</option><option value=\"1\">2</option><option value=\"2\">3</option><option value=\"3\">4</option><option value=\"4\">5</option><option value=\"5\">6</option><option value=\"6\">7</option><option value=\"7\">8</option></select>";
+		input_html = `<select name="channel${i}" id="channel${i}" `;
+		input_html += "class=\"channel_select\"><option value=\"0\">1</option><option value=\"1\">2</option><option value=\"2\">3</option><option value=\"3\">4</option><option value=\"4\">5</option><option value=\"5\">6</option><option value=\"6\">7</option><option value=\"7\">8</option></select>";
 		cell.innerHTML = input_html;
 		cell.lastChild.onclick = function () {
 			set_value_pending(this);
@@ -171,7 +172,7 @@ function create_pilots() {
 
 		// RSSI threshold
 		cell = row.insertCell(-1);
-		input_html = "<input type=\"number\" id=\"RSSIthreshold" + i + "\" min=\"0\" max=\"342\" step=\"1\" class=\"rssi_select\">";
+		input_html = `<input type="number" id="RSSIthreshold${i}" min="0" max="342" step="1" class="rssi_select">`;
 		cell.innerHTML = input_html;
 		cell.lastChild.onclick = function () {
 			set_value_pending(this);
@@ -179,7 +180,7 @@ function create_pilots() {
 		}
 		// Enabled checkbox
 		cell = row.insertCell(-1);
-		input_html= "<input type='checkbox' id='pilot_enabled_" + i + "'>";
+		input_html= `<input type="checkbox" id="pilot_enabled_${i}">`;
 		cell.innerHTML = input_html;
 		cell.lastChild.onclick = function () {
 			set_value_pending(this);
@@ -188,7 +189,7 @@ function create_pilots() {
 
 		// multiplex checkbox
 		cell = row.insertCell(-1);
-		input_html = "<input type='checkbox' id='pilot_multuplex_off_" + i + "'>";
+		input_html = `<input type="checkbox" id="pilot_multuplex_off_${i}">`;
 		cell.innerHTML = input_html;
 		cell.lastChild.onclick = function () {
 			set_value_pending(this);
