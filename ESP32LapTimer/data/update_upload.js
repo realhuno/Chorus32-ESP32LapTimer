a7fbc220feafe0d9ed1070a8996b6d0b316e502c
Kevin <smeatsan@gmail.com>
2019-10-18 15:08:31 +0200

    Add missing javascript file

---
+++ ESP32LapTimer/data/update_upload.js
@@ -0,0 +1,41 @@
+var button = document.getElementById("update_button");
+
+button.addEventListener("click", upload_firmware);
+
+function upload_progress(e) {
+	var progress = (e.loaded/ e.total);
+	var elem = document.getElementById("update_bar");
+	var percent = parseInt(progress*100);
+	elem.style.width = percent + "%";
+	elem.innerHTML = percent + "%";
+}
+
+function upload_complete(e) {
+	console.log("Upload complete!");
+}
+
+function upload_failed(e) {
+	console.log("Upload failed!");
+}
+
+function upload_firmware() {
+	var file = document.getElementById("update_file").files[0];
+	
+	var formData = new FormData();
+	formData.append("update", file);
+	
+	var xhr = new XMLHttpRequest();
+	xhr.upload.addEventListener("progress", upload_progress, false);
+	xhr.addEventListener("load", upload_complete, false);
+	xhr.addEventListener("error", upload_failed, false);
+	xhr.addEventListener("abort", upload_failed, false);
+	
+	xhr.onreadystatechange = function () {
+		if(xhr.readyState === 4) {
+			document.write(xhr.responseText);
+		}
+	};
+	
+	xhr.open('POST', '/update', true);
+	xhr.send(formData);
+}
