c88b28d0e0da33682e6cc7d9cd4e54018f931e14
Kevin <smeatsan@gmail.com>
2019-11-18 17:22:30 +0100

    Add build js script.

---
+++ ESP32LapTimer/build_js.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+TERSER_CMD="${HOME}/node_modules/terser/bin/terser"
+cat ${SCRIPT_DIR}/Comms.cpp | grep "^#define" | grep "RESPONSE_\|CONTROL_\|EXTENDED_\|BINARY_" | awk '{print "export const " $2 " = " $3 ";"}' > ${SCRIPT_DIR}/data_src/constants.js
+
+for js_file in ${SCRIPT_DIR}/data_src/*.js; do
+	${TERSER_CMD} -c toplevel -m reserved,toplevel,eval -o ${SCRIPT_DIR}/data/$(basename $js_file) $js_file
+done
--- ESP32LapTimer/generate_js_const.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-cat ${SCRIPT_DIR}/Comms.cpp | grep "^#define" | grep "RESPONSE_\|CONTROL_\|EXTENDED_\|BINARY_" | awk '{print "export const " $2 " = " $3 ";"}' > ${SCRIPT_DIR}/data/constants.js
