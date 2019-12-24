f8070ab0591829c1291c0d64b5a5414550ee5ec2
Kevin <smeatsan@gmail.com>
2019-12-24 15:13:19 +0100

    Fix build_js

--- ESP32LapTimer/build_js.sh
+++ ESP32LapTimer/build_js.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
 TERSER_CMD="${HOME}/node_modules/terser/bin/terser"
-cat ${SCRIPT_DIR}/Comms.cpp | grep "^#define" | grep "RESPONSE_\|CONTROL_\|EXTENDED_\|BINARY_" | awk '{print "export const " $2 " = " $3 ";"}' > ${SCRIPT_DIR}/data_src/constants.js
+cat ${SCRIPT_DIR}/src/Comms.cpp | grep "^#define" | grep "RESPONSE_\|CONTROL_\|EXTENDED_\|BINARY_" | awk '{print "export const " $2 " = " $3 ";"}' > ${SCRIPT_DIR}/data_src/constants.js
 
 for js_file in ${SCRIPT_DIR}/data_src/*.js; do
 	${TERSER_CMD} -c toplevel -m reserved,toplevel,eval -o ${SCRIPT_DIR}/data/$(basename $js_file) $js_file
