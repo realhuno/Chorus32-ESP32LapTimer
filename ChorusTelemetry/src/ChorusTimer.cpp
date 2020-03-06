12840e3c112cd83169934a2d8b1e5c28221e0c86
Kevin <smeatsan@gmail.com>
2020-03-06 17:44:30 +0100

    [WIP] Add support for proxy commands in telemetry script.

--- ChorusTelemetry/src/ChorusTimer.cpp
+++ ChorusTelemetry/src/ChorusTimer.cpp
@@ -6,8 +6,13 @@
 
 #define WIFI_AP_NAME "Chorus32 LapTimer"
 
-#define UART_TX 21
-#define UART_RX 12
+#define UART_TX 15
+#define UART_RX 4
+
+
+#define PROXY_PREFIX 'P' // used to configure any proxy (i.e. this device ;))
+#define PROXY_CONNECTION_STATUS 'c'
+#define PROXY_WIFI_STATUS 'w'
 
 WiFiClient client;
 
@@ -23,20 +28,10 @@ void chorus_connect() {
 	}
 }
 
-void wifi_connect() {
-	WiFi.begin(WIFI_AP_NAME);
-	while (WiFi.status() != WL_CONNECTED) {
-		delay(500);
-		Serial.print(".");
-	}
-	Serial.println("Connected!");
-	Serial.print("IP: ");
-	Serial.println(WiFi.localIP());
-}
-
 void setup() {
 	Serial.begin(115200);
-	Serial1.begin(115200, SERIAL_8N1, UART_TX, UART_RX, true);
+	Serial1.begin(115200, SERIAL_8N1, UART_RX, UART_TX, true);
+	WiFi.begin(WIFI_AP_NAME);
 	delay(500);
 
 	memset(buf, 0, 1500 * sizeof(char));
@@ -46,6 +41,7 @@ void loop() {
 	while(client.connected() && client.available()) {
 		// Doesn't include the \n itself
 		String line = client.readStringUntil('\n');
+
 		// Only forward specific messages for now
 		if(line[2] == 'L') {
 			Serial1.println(line);
@@ -58,18 +54,26 @@ void loop() {
 			buf_pos = 0; // clear buffer when full
 			break;
 		}
-		buf[buf_pos++] = Serial.read();
+		buf[buf_pos++] = Serial1.read();
 		if(buf[buf_pos - 1] == '\n') {
-			Serial.print("Forwarding to chorus: ");
-			Serial.write((uint8_t*)buf, buf_pos);
-			client.write(buf, buf_pos);
+			// catch proxy command
+			if(buf[0] == 'P') {
+				switch(buf[3]) {
+					case PROXY_WIFI_STATUS:
+						Serial1.printf("%cS*%c%1x\n", PROXY_PREFIX, PROXY_WIFI_STATUS, WiFi.status());
+						break;
+					case PROXY_CONNECTION_STATUS:
+						Serial1.printf("%cS*%c%1x\n", PROXY_PREFIX, PROXY_CONNECTION_STATUS, client.connected());
+						break;
+				}
+			}
+			else if((buf[0] == 'E' && (buf[1] == 'S' || buf[1] == 'R')) || buf[0] == 'S' || buf[0] == 'R') {
+				Serial.print("Forwarding to chorus: ");
+				Serial.write((uint8_t*)buf, buf_pos);
+				client.write(buf, buf_pos);
+			}
 			buf_pos = 0;
 		}
-	}
-
-	if(!WiFi.isConnected()) {
-		Serial.println("Lost WiFi connection! Trying to reconnect");
-		wifi_connect();
 	}
 
 	if(!client.connected()) {
