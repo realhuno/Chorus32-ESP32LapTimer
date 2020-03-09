2a057147186fe5777db2ed0aa014b9af74845aac
Kevin <smeatsan@gmail.com>
2020-03-09 19:40:00 +0100

    Disable update firmware button until a file is selected

--- ESP32LapTimer/data_src/update_upload.js
+++ ESP32LapTimer/data_src/update_upload.js
@@ -1,6 +1,11 @@
 var button = document.getElementById("update_button");
-
+var upload_select = document.getElementById("update_file");
 button.addEventListener("click", upload_firmware);
+button.disabled = true;
+upload_select.addEventListener("input", function() {
+	button.disabled = false;
+});
+
 
 function upload_progress(e) {
 	var progress = (e.loaded/ e.total);
@@ -20,22 +25,22 @@ function upload_failed(e) {
 
 function upload_firmware() {
 	var file = document.getElementById("update_file").files[0];
-	
+
 	var formData = new FormData();
 	formData.append("update", file);
-	
+
 	var xhr = new XMLHttpRequest();
 	xhr.upload.addEventListener("progress", upload_progress, false);
 	xhr.addEventListener("load", upload_complete, false);
 	xhr.addEventListener("error", upload_failed, false);
 	xhr.addEventListener("abort", upload_failed, false);
-	
+
 	xhr.onreadystatechange = function () {
 		if(xhr.readyState === 4) {
 			document.write(xhr.responseText);
 		}
 	};
-	
+
 	xhr.open('POST', '/update', true);
 	xhr.send(formData);
 }
