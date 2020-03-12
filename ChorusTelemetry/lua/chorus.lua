e074638e028bebf6359b6362f7e8830fd1423e57
Kevin <smeatsan@gmail.com>
2020-03-12 21:02:37 +0100

    [WIP] Add more settings to the lua script

--- ChorusTelemetry/lua/chorus.lua
+++ ChorusTelemetry/lua/chorus.lua
@@ -6,24 +6,12 @@ assert(loadScript(SCRIPT_HOME.."/MSP/common.lua"))()
 
 local MSP_ADD_LAP = 229
 
-local PAGE_RACE = 0
-local PAGE_SETTINGS = 1
-
-local SETTING_RX_NUM = 0
-local SETTING_ADC_TYPE = 1
-local SETTING_ADC_VAL = 2
-local SETTING_CUTOFF = 3
-
-local SETTING_NUM = 4
-
-local current_screen = 0
+local current_screen = 1
 local current_item = 0
 local is_editing = false
 
 local msp_string = "Msp msg: none"
 local debug_string = "debug string!"
-
--- settings
 
 local last_cmd = ""
 
@@ -44,6 +32,9 @@ local CHORUS_CMD_ADC_VAL = 3
 local CHORUS_CMD_WIFI_PROTO = 4
 local CHORUS_CMD_WIFI_CHANNEL = 5
 local CHORUS_CMD_FILTER_CUTOFF = 6
+local CHORUS_CMD_MIN_LAPTIME = 7
+local CHORUS_CMD_SOUNDS = 8
+local CHORUS_CMD_SKIP_FIRST = 9
 local chorus_cmds = {}
 
 chorus_cmds[CHORUS_CMD_NUM_RX] =  { cmd="ER*M", bits=4, last_val=nil }
@@ -52,28 +43,74 @@ chorus_cmds[CHORUS_CMD_ADC_VAL] =  { cmd="ER*V", bits=16, last_val=nil }
 chorus_cmds[CHORUS_CMD_WIFI_PROTO] =  { cmd="ER*w", bits=4, last_val=nil }
 chorus_cmds[CHORUS_CMD_WIFI_CHANNEL] =  { cmd="ER*W", bits=4, last_val=nil }
 chorus_cmds[CHORUS_CMD_FILTER_CUTOFF] =  { cmd="ER*F", bits=16, last_val=nil }
+chorus_cmds[CHORUS_CMD_MIN_LAPTIME] =  { cmd="R*M", bits=8, last_val=nil }
+chorus_cmds[CHORUS_CMD_SOUNDS] =  { cmd="R*S", bits=4, last_val=nil }
+chorus_cmds[CHORUS_CMD_SKIP_FIRST] =  { cmd="R*1", bits=4, last_val=nil }
 
 local settings_labels = {}
 local settings_fields = {}
 
-local settings_x_label = 10
-settings_labels[#settings_labels +1] = {t="Num Rx", x=settings_x_label, y=10}
-settings_labels[#settings_labels + 1] = {t="ADC Type", x=settings_x_label, y=20}
-settings_labels[#settings_labels + 1] = {t="ADC Val", x=settings_x_label, y=30}
-settings_labels[#settings_labels + 1] = {t="WiFi Channel", x=settings_x_label, y=40}
-settings_labels[#settings_labels + 1] = {t="WiFi Protocol", x=settings_x_label, y=50}
-settings_labels[#settings_labels + 1] = {t="Filter cutoff", x=settings_x_label, y=60}
+local y = 0
+local x = 0
+local function inc_y(val)
+	y = y + val
+	return y
+end 
+local linespacing = 9
+settings_labels[#settings_labels +1] = {t="Num Rx", x=x, y=inc_y(linespacing)}
+settings_labels[#settings_labels + 1] = {t="ADC Type", x=x, y=inc_y(linespacing)}
+settings_labels[#settings_labels + 1] = {t="ADC Val", x=x, y=inc_y(linespacing)}
+settings_labels[#settings_labels + 1] = {t="WiFi Protocol", x=x, y=inc_y(linespacing)}
+settings_labels[#settings_labels + 1] = {t="WiFi Channel", x=x, y=inc_y(linespacing)}
+settings_labels[#settings_labels + 1] = {t="Filter cutoff", x=x, y=inc_y(linespacing)}
+y = 0
+x = 70
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 1, max = 6, cmd_id=CHORUS_CMD_NUM_RX}
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 0, max = 3, labels={"OFF", "ADC5", "ADC6", "INA219"}, cmd_id=CHORUS_CMD_ADC_TYPE}
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 1, max = 10000, cmd_id=CHORUS_CMD_ADC_VAL}
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 0, max = 1, labels={"bgn", "b"}, cmd_id=CHORUS_CMD_WIFI_PROTO}
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 1, max = 13, cmd_id=CHORUS_CMD_WIFI_CHANNEL}
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 1, max = 10000, cmd_id=CHORUS_CMD_FILTER_CUTOFF}
+x = 100
+y = 0
+settings_labels[#settings_labels + 1] = {t="Min Laptime", x=x, y=inc_y(linespacing)}
+settings_labels[#settings_labels + 1] = {t="Device Sounds", x=x, y=inc_y(linespacing)}
+settings_labels[#settings_labels + 1] = {t="Skip First Lap", x=x, y=inc_y(linespacing)}
 
-local settings_x_fields = 100
-settings_fields[#settings_fields + 1] = {x= settings_x_fields, y=10, min = 1, max = 6, cmd_id=CHORUS_CMD_NUM_RX}
-settings_fields[#settings_fields + 1] = {x= settings_x_fields, y=20, min = 0, max = 3, labels={"OFF", "ADC5", "ADC6", "INA219"}, cmd_id=CHORUS_CMD_ADC_TYPE}
-settings_fields[#settings_fields + 1] = {x= settings_x_fields, y=30, min = 1, max = 10000, cmd_id=CHORUS_CMD_ADC_VAL}
-settings_fields[#settings_fields + 1] = {x= settings_x_fields, y=40, min = 0, max = 1, labels={"bgn", "b"}, cmd_id=CHORUS_CMD_WIFI_PROTO}
-settings_fields[#settings_fields + 1] = {x= settings_x_fields, y=50, min = 1, max = 13, cmd_id=CHORUS_CMD_WIFI_CHANNEL}
+
+y = 0
+x= 170
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 0, max = 254, cmd_id=CHORUS_CMD_MIN_LAPTIME}
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 0, max = 1, labels={"OFF","ON"}, cmd_id=CHORUS_CMD_SOUNDS}
+settings_fields[#settings_fields + 1] = {x= x, y=inc_y(linespacing), min = 0, max = 1, labels={"OFF","ON"}, cmd_id=CHORUS_CMD_SKIP_FIRST}
+
 
 local settings_page = { title = "Chorus32 Settings", labels=settings_labels, fields=settings_fields}
 
-local pages = {settings_page}
+local race_labels = {}
+
+local function ms_to_string(ms)
+	local seconds = ms/1000
+	local minutes = math.floor(seconds/60)
+	seconds = seconds % 60
+	return  (string.format("%02d:%05.2f", minutes, seconds))
+end
+
+local function draw_race_screen()
+	lcd.drawScreenTitle("Racing", 1, 2)
+	lcd.drawText(0, 11, "WiFi status:", 0)
+	lcd.drawText(lcd.getLastPos()+2, 11, wifi_status,0)
+	lcd.drawText(lcd.getLastPos()+2, 11, ms_to_string(last_lap_int),0)
+	lcd.drawText(0, 21, "Connection status:", 0)
+	lcd.drawText(lcd.getLastPos()+2, 21, connection_status,0)
+	lcd.drawText(0, 31, "Last rx: ",0)
+	lcd.drawText(lcd.getLastPos()+2, 31, last_cmd,0)
+	lcd.drawText(0, 41, msp_string,0)
+	lcd.drawText(0, 51, debug_string,0)
+end
+
+local race_page = { custom_draw_func=draw_race_screen }
+local pages = {race_page, settings_page}
 
 local last_chorus_update = 0
 
@@ -120,29 +157,15 @@ local function convert_lap(pilot, lap, laptime)
 	return values
 end
 
-local function ms_to_string(ms)
-	local seconds = ms/1000
-	local minutes = math.floor(seconds/60)
-	seconds = seconds % 60
-	return  (string.format("%02d:%05.2f", minutes, seconds))
-end
-
 
 local function process_extended_cmd(cmd)
-		local node = string.sub(cmd, 2,2)
-		local chorus_cmd = string.sub(cmd, 3,3)
-		-- TODO: make this more generic
-		if (chorus_cmd == "M") then
-			chorus_cmds[CHORUS_CMD_NUM_RX].last_val = tonumber(string.sub(cmd, 4), 16)
-		elseif (chorus_cmd == "v") then
-			chorus_cmds[CHORUS_CMD_ADC_TYPE].last_val = tonumber(string.sub(cmd, 4), 16)
-		elseif (chorus_cmd == "V") then
-			chorus_cmds[CHORUS_CMD_ADC_VAL].last_val = tonumber(string.sub(cmd, 4), 16)
-		elseif (chorus_cmd == "w") then
-			chorus_cmds[CHORUS_CMD_WIFI_PROTO].last_val = tonumber(string.sub(cmd, 4), 16)
-		elseif (chorus_cmd == "W") then
-			chorus_cmds[CHORUS_CMD_WIFI_CHANNEL].last_val = tonumber(string.sub(cmd, 4), 16)
+	local node = string.sub(cmd, 2,2)
+	local chorus_cmd = string.sub(cmd, 3,3)
+	for i=1,#chorus_cmds do
+		if string.sub(chorus_cmds[i].cmd, 1, 1) == "E" and string.sub(chorus_cmds[i].cmd, 4, 4) == chorus_cmd then
+			chorus_cmds[i].last_val = tonumber(string.sub(cmd, 4), 16)
 		end
+	end
 end
 
 local function process_proxy_cmd(cmd)
@@ -175,21 +198,13 @@ local function process_cmd(cmd)
 			lap_sent = false
 			last_lap_ack_needed = last_lap_ack_needed + 1
 		end
+		for i=1,#chorus_cmds do
+			if string.sub(chorus_cmds[i].cmd, 3, 3) == chorus_cmd then
+				chorus_cmds[i].last_val = tonumber(string.sub(cmd, 4), 16)
+			end
+		end
 
 	end
-end
-
-local function draw_race_screen()
-	lcd.drawScreenTitle("Racing", 1, 2)
-	lcd.drawText(0, 11, "WiFi status:", 0)
-	lcd.drawText(lcd.getLastPos()+2, 11, wifi_status,0)
-	lcd.drawText(lcd.getLastPos()+2, 11, ms_to_string(last_lap_int),0)
-	lcd.drawText(0, 21, "Connection status:", 0)
-	lcd.drawText(lcd.getLastPos()+2, 21, connection_status,0)
-	lcd.drawText(0, 31, "Last rx: ",0)
-	lcd.drawText(lcd.getLastPos()+2, 31, last_cmd,0)
-	lcd.drawText(0, 41, msp_string,0)
-	lcd.drawText(0, 51, debug_string,0)
 end
 
 local function check_setting_pos(setting)
@@ -208,17 +223,16 @@ local function check_blinking(item)
 end
 
 local function draw_screen()
-	-- workaround for old screen
-	if current_screen == 0 then
-		draw_race_screen()
+	local page = pages[current_screen]
+	if page.custom_draw_func ~= nil then
+		page.custom_draw_func()
 		return
 	end
-	local page = pages[current_screen]
 	local labels = page.labels
 	local fields = page.fields
 	lcd.drawScreenTitle(page.title, 2, 2)
 	for i=1,#labels do
-		lcd.drawText(labels[i].x, labels[i].y, labels[i].t, 0)
+		lcd.drawText(labels[i].x, labels[i].y, labels[i].t, SMLSIZE)
 	end
 
 	for i=1,#fields do
@@ -236,7 +250,7 @@ local function draw_screen()
 				last_chorus_update = getTime()
 			end
 		end
-		lcd.drawText(fields[i].x, fields[i].y, field_text, check_blinking(i))
+		lcd.drawText(fields[i].x, fields[i].y, field_text, check_blinking(i) + SMLSIZE)
 	end
 end
 
@@ -272,7 +286,10 @@ local function handle_input(event)
 		handle_input_edit(event)
 	elseif(event == EVT_PAGE_BREAK) then
 		current_item = 1
-		current_screen = (current_screen + 1) % 2
+		current_screen = (current_screen + 1)
+		if current_screen > #(pages) then
+			current_screen = 1
+		end
 	elseif(event == EVT_MINUS_BREAK) then
 		current_item = current_item + 1
 		if(current_item > #(pages[current_screen].fields)) then
@@ -295,26 +312,21 @@ end
 local run = function (event)
 	handle_input(event)
 	draw_ui()
-
---	if serialReadLine ~= nil then
+	--if type(serialReadLine) == "function" then
 	local rx = serialReadLine()
---	end
+	--end
 	if(rx ~= nil) then
 		last_cmd = rx
 		process_cmd(last_cmd)
 	end
 
 	if(last_sent + send_interval < getTime()) then
-		--if(last_lap_ack_needed > 0) then
-		--	send_msp(last_lap)
-		--end
 		last_sent = getTime()
 		get_connection_status()
 	end
 	mspProcessTxQ()
 	if last_lap_int ~= 0 then
 		if lap_sent == false then
-			--send_msp(last_lap)
 			if protocol.mspWrite(MSP_ADD_LAP, last_lap) ~= nil then
 				lap_sent = true
 				debug_string = "Lap sent!"
