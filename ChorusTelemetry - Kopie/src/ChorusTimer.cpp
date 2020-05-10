5a44fc298ed8594300da1ed9fc2f88aa7f8d6ed7
Patric Hainz <patric.hainz@gmail.com>
2020-05-10 09:24:00 +0200

    ADD OTA and RSSI

--- ChorusTelemetry - Kopie/src/ChorusTimer.cpp
+++ ChorusTelemetry - Kopie/src/ChorusTimer.cpp
@@ -1,14 +1,9 @@
 #include <WiFi.h>
 #include <WiFiMulti.h>
 #include <esp_wifi.h>
+
 #include <lwip/sockets.h>
 #include <lwip/netdb.h>
-
-//OTA
-#include <ESPmDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
-
 
 #define WIFI_AP_NAME "Chorus32 LapTimer"
 
@@ -80,38 +75,9 @@ void setup() {
 	delay(500);
 
 	memset(buf, 0, 1500 * sizeof(char));
-	//OTA
-	  ArduinoOTA
-    .onStart([]() {
-      String type;
-      if (ArduinoOTA.getCommand() == U_FLASH)
-        type = "sketch";
-      else // U_SPIFFS
-        type = "filesystem";
-
-      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
-      Serial.println("Start updating " + type);
-    })
-    .onEnd([]() {
-      Serial.println("\nEnd");
-    })
-    .onProgress([](unsigned int progress, unsigned int total) {
-      Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-    })
-    .onError([](ota_error_t error) {
-      Serial.printf("Error[%u]: ", error);
-      if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-      else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-      else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-      else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-      else if (error == OTA_END_ERROR) Serial.println("End Failed");
-    });
-
-  ArduinoOTA.begin();
 }
 
 void loop() {
-	ArduinoOTA.handle();
 	real_rssi=WiFi.RSSI();
     if(wifiMulti.run() != WL_CONNECTED) {
         Serial.println("WiFi not connected!");
